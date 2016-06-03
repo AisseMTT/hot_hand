985724da375f585717e30330f2079008bc8e9746
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-06-02 17:22:13 -0700

    added runs test

--- analysis_helpers.R
+++ analysis_helpers.R
@@ -118,3 +118,43 @@ populate_prev_shots <- function(d) {
 #   
 #   res_d
 # }
+
+########
+######## calc_runs()
+######## ---------------------
+######## Calcuate statistic and p-value of Wald-Wolfowitz run test
+######## Code adopted from runs.pvalue from randomizeBE
+calc_runs <- function(y, pmethod = c("exact", "normal", "cc")) {
+  pmethod <- match.arg(pmethod)
+  y <- na.omit(y)
+  yuniq <- unique(y)
+  if (length(yuniq) == 2) {
+    s <- as.numeric(y)
+    s[yuniq[1] == y] <- -1
+    s[yuniq[2] == y] <- +1
+  }
+  else {
+    med <- median(y, na.rm = TRUE)
+    s <- sign(y - med)
+    s[s == 0] <- +1
+  }
+  n <- length(s)
+  R <- 1 + sum(as.numeric(s[-1] != s[-n]))
+  n1 <- sum(s == +1)
+  n2 <- sum(s == -1)
+  n <- n1 + n2
+  E <- 1 + 2 * n1 * n2/n
+  s2 <- (2 * n1 * n2 * (2 * n1 * n2 - n))/(n^2 * (n - 1))
+  ccf <- 0
+  if (pmethod == "cc" | pmethod == "exact") {
+    ccf <- ifelse((R - E) < 0, +0.5, -0.5)
+  }
+  statistic <- (R - E + ccf)/sqrt(s2)
+  if ((n > 30 & n1 > 12 & n2 > 12) | pmethod != "exact") {
+    pvalue <- 2 * pnorm(-abs(statistic))
+  }
+  else {
+    pvalue <- pruns.exact(R, n1, n2, tail = "2-sided")
+  }
+  c(statistic, pvalue)
+}
